375c0d8c12e8272b199a10e930384fc9a9b17905
lalebdi <loai.alebdi@gmail.com>
2020-09-02 12:59:23 -0400

    done with the page footer

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -4,12 +4,12 @@ export default function PageFooter() {
   return (
     <div id="footer">
       <ul className="copyright">
-        <li>&copy; Untitled. All rights reserved.</li>
+        <li>&copy; Leah Web Dev Portfolio site. All rights reserved.</li>
         <li>
-          Design: <a href="http://html5up.net">HTML5 UP</a>
+          Designed using Gatby.
         </li>
         <li>
-          Demo Images: <a href="http://unsplash.com/">Unsplash</a>
+              Images: <a href="http://unsplash.com/">Unsplash</a>
         </li>
       </ul>
     </div>
