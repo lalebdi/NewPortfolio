8cc871eb247b766a00080e440670551afd7a4377
lalebdi <loai.alebdi@gmail.com>
2020-09-06 15:22:43 -0400

    fixed security vulnerabilities

--- src/pages/index.js
+++ src/pages/index.js
@@ -287,7 +287,7 @@ const IndexPage = () => {
                   <p>Stack : React, Socket io, React-Emoji, React-Router, Cors, Node, and Express.</p>
                   <button><a href="https://github.com/lalebdi/chatApp" target="_blank" rel="noreferrer" >Source Code</a></button>
                   <br/>
-                  <button className="live-btn" ><a href="https://5f529d2bfd13113312e00abd--friendly-borg-6726ab.netlify.app" target="_blank">Live Site </a></button>
+                  <button className="live-btn" ><a href="https://5f529d2bfd13113312e00abd--friendly-borg-6726ab.netlify.app" target="_blank" rel="noreferrer" >Live Site </a></button>
                   </aside>
                   <img className="demo" src={gif5} alt="make awesome projects" />
                   </div>
